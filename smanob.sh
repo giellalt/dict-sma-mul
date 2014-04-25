86de188e0fdf75f3710cf9b2ce0500f69f6ebd33
Trond Trosterud <trond.trosterud@uit.no>
2014-04-25 12:34:39 +0000

    new version of script.

--- smanob.sh
+++ smanob.sh
@@ -1,35 +1,45 @@
 
 # Dette skal bli ei makefil for å lage smanob.fst, 
-# ei fst-fil som tar sma og gjev ei nob-omsetjing.
+# ei fst-fil som tar sme og gjev ei fin-omsetjing.
 
 # Førebels er det berre eit shellscript.
-# Kommando for å lage smanob.fst
 
-# sh smanob.sh 
-
-echo 
-echo "Etter at dette scriptet er ferdig står du i xfst med promten"
-echo "xfst[1]"
-echo 
-echo "Gjör då dette:"
-echo "invert"
-echo "save bin/smanob.fst"
-echo "quit"
 echo ""
-echo "LEXICON Root" > bin/smanob_sh.lexc
+echo ""
+echo "---------------------------------------------------"
+echo "Shellscript to make a transducer of the dictionary."
+echo ""
+echo "It writes a lexc file to bin, containing the line	 "
+echo "LEXICON Root										 "
+echo "Thereafter, it picks lemma and first translation	 "
+echo "of the dictionary, adds them to this lexc file,	 "
+echo "and compiles a transducer bin/smanob.fst		 "
+echo ""
+echo "Usage:"
+echo "lookup bin/smanob.fst"
+echo "---------------------------------------------------"
+echo ""
+echo ""
 
-cat  src/*_smanob.xml | \
-egrep -v '<(lc|lsub|analysis)>' | \
-egrep -v '<lemma_ref' | \
-egrep -v '<stem ' | \
-tr '\n' '™' | sed 's/<e/£/g;'| tr '£' '\n'| \
-sed 's/<re>[^>]*>//g;'|tr '<' '>'| cut -d">" -f6,16| \
-tr ' ' '_'| sed 's/:/%/g;'|tr '>' ':'| \
-grep -v '__'| \
-grep -v 'xml-stylesheet_alternate' | \
-sed 's/$/ # ;/g' >> bin/smanob_sh.lexc
+echo "LEXICON Root" > bin/smanob.lexc
+cat src/*_smanob.xml | \
+grep '^ *<[lt][ >]'  | \
+sed 's/^ *//g;'      | \
+sed 's/<l /™/g;'     | \
+tr '\n' '£'          | \
+sed 's/£™/€/g;'      | \
+tr '€' '\n'          | \
+tr '<' '>'           | \
+cut -d'>' -f2,6      | \
+tr '>' ':'           | \
+tr ' ' '_'           | \
+sed 's/$/ # ;/g;'    >> bin/smanob.lexc        
 
-xfst -e "read lexc < bin/smanob_sh.lexc"
+#xfst -e "read lexc < bin/smanob.lexc"
 
-
-
+printf "read lexc < bin/smanob.lexc \n\
+invert net \n\
+save stack bin/smanob.fst \n\
+quit \n" > tmpfile
+xfst -utf8 < tmpfile
+rm -f tmpfile
